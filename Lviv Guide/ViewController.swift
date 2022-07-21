a5dd7e2a39cfd33f70ba7d361a6c724aa50f2e77
olhaSusla <olga.susla13@gmail.com>
2022-07-21 13:12:34 +0300

    Initial
    Inintial comit

---
+++ Lviv Guide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lviv Guide
+//
+//  Created by Ольга on 21.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
